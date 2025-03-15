85c16601bd40d33b175e3532aff2489a5ecf5494
Mitch Brown <mitchbrown1024@gmail.com>
2025-03-15 12:55:59 -0700

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Mitch Brown on 2025-03-15.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
